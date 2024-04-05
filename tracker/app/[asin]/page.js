fb0851e3d437808164675e6bb3dae45c4d2cd6d9
krishvsoni <sonikrish994@gmail.com>
2024-04-05 16:02:11 +0530

    dependancies fixed

--- tracker/app/[asin]/page.js
+++ tracker/app/[asin]/page.js
@@ -1,40 +1,75 @@
-import React from "react";
-import { Chart } from "react-charts";
+'use client';
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import DisplayChart from "../../components/DisplayChart";
-export default function Page({params}){
-    const data = [
-        {
-          label: 'React Charts',
-          data: [
-            {
-              date: 33,
-              stars: 123,
-            },
-            {
-              date: 45,
-              stars: 456,
-            },
-            {
-              data:44,
-              stars:2023
-            },
-            {
-              date: 23,
-              stars: 356
-            }
-          ]
-        }
-      ];
-      
+import MongoClient from 'mongodb';
 
-    
-        return (
-          <>
-            <Navbar/>
-            Track Price for: {params.asin}
-            <DisplayChart data={data}/>
-    
-          </>
-        );
-    }
+let client;
+if (typeof window === 'undefined') {
+  const url = process.env.MongoDB;
+  client = new MongoClient(url);
+}
+
+
+const dbName = 'amazon_data';
+
+export default async function Page({ params }) {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('products');
+        let asin = params.asin;
+
+        const data = await collection.aggregate([
+          {
+            $match: {
+              asin: asin.toUpperCase()
+            }
+          },
+          {
+            $group: {
+              _id: "$asin",
+              data: {
+                $push: {
+                  date: { $dayOfMonth: "$time" },
+                  price: { $toInt: "$price" }
+                }
+              }
+            }
+          },
+          {
+            $project: {
+              _id: 0,
+              label: { $literal: "Product" },
+              data: 1
+            }
+          }
+        ]).toArray();
+
+        console.log(data);
+        setChartData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      client.close();
+    };
+  }, [params.asin]);
+
+  return (
+    <>
+      <Navbar />
+      Track Price for: {params.asin}
+      <DisplayChart data={chartData} />
+    </>
+  );
+}
--- tracker/components/DisplayChart.js
+++ tracker/components/DisplayChart.js
@@ -1,35 +1,44 @@
 import React from "react";
-import { Chart } from "react-charts";
+import { Line } from "react-chartjs-2";
 
-const DisplayChart = (data) => { 
-        
-    const primaryAxis = React.useMemo(
-        () => ({
-          getValue: datum => datum.date,
-        }),
-        []
-      )
-    
-      const secondaryAxes = React.useMemo(
-        () => [
-          {
-            getValue: datum => datum.stars,
-          },
-        ],
-        []
-      )
-    return (
-        <div> 
-            Track Price for: {title} 
-            <div className="size-64">
-                <Chart options={{ 
-                    data,
-                    primaryAxis,
-                    secondaryAxes
-                }} />
-            </div>
-        </div> 
-    ); 
+const DisplayChart = ({ data, title }) => {
+  const chartData = {
+    labels: data.map(entry => entry.date),
+    datasets: [
+      {
+        label: "Price",
+        data: data.map(entry => entry.price),
+        borderColor: "rgba(75,192,192,1)",
+        backgroundColor: "rgba(75,192,192,0.4)",
+        fill: false
+      }
+    ]
+  };
+
+  const options = {
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'day'
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  return (
+    <div> 
+      Track Price for: {title} 
+      <div className="size-64">
+        <Line data={chartData} options={options} />
+      </div>
+    </div>
+  ); 
 };
 
 export default DisplayChart;
